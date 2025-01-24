fc06b6b40795353e86667dad1f45ebecab0432d7
Sharif-Miah <farhansharif1410@gmail.com>
2025-01-24 12:31:56 +0600

    project setup done

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       container: {
         center: true,
         padding: {
-          DEFAULT: '1rem',
+          DEFAULT: "1rem",
         },
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
